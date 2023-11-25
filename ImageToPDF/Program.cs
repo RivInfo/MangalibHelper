562be2a4e30840ec86e1455d2535c8afac2c56c3
Роман Иванов <romaiv99@gmail.com>
2023-11-25 23:40:47 +0300

    Декомпозиция, перевод gif в изображение, можно создавать всё в один файл или делить по главам

--- ImageToPDF/Program.cs
+++ ImageToPDF/Program.cs
@@ -2,149 +2,58 @@
 using System.Globalization;
 using System.IO.Compression;
 using System.Text.RegularExpressions;
+using ImageToPdfClassLibrary.Archive;
+using ImageToPdfClassLibrary.OutFile;
 using iText.IO.Image;
 using iText.Kernel.Pdf;
 using iText.Layout;
 using iText.Layout.Element;
+using iText.StyledXmlParser.Jsoup.Select;
 
-Console.WriteLine("Поместите архивы в открытую папку, " +
+Console.WriteLine("Поместите архивы в открывшуюся папку, " +
                   "именование должно быть цифрами в порядке глав");
 
-const string ArchiveFolderName = "ArchiveFolder";
-
-DirectoryInfo info = Directory.CreateDirectory(ArchiveFolderName);
+DirectoryInfo info = Directory.CreateDirectory(ArchiveCollection.ArchiveFolderName);
 
 Process.Start(new ProcessStartInfo { FileName = info.FullName, UseShellExecute = true });
 
-Console.WriteLine("После добавления фалов. Нажмите на ввод, чтобы продолжить.");
+Console.WriteLine("После добавления файлов. Нажмите на ввод, чтобы продолжить.");
 
 Console.ReadLine();
 
-string[] files = Directory.GetFiles(info.FullName);
+ArchiveCollection archiveCollection = new();
 
-SortedDictionary<double, string> useFile = new SortedDictionary<double, string>();
+archiveCollection.InitFiles();
 
-const string Pattern = @"(Том (\d+))|(Глава (\d+(\.\d+)?))";
-
-foreach (var file in files)
+if (!archiveCollection.IsAnyFiles)
 {
-    string fileName = file.Split('\\')[^1];
-    
-    if (!fileName.EndsWith(".zip") || !fileName.EndsWith(".rar"))
-    {
-        fileName = fileName.Remove(fileName.Length - 4);
-    }
-    else
-    {
-        Console.WriteLine("Файл не архив: " + file);
-        continue;
-    }
-
-    Regex rgx = new Regex(Pattern);
-    
-    MatchCollection matchesNums = rgx.Matches(fileName);
-
-    if (matchesNums.Count != 2)
-    {
-        Console.WriteLine("В названии файла нет 2 чисел: " + fileName);
-        continue;
-    }
-
-    double orderNumber = 0;
-    
-    if (int.TryParse(matchesNums[0].Groups[2].Value, out int tom))
-    {
-        orderNumber += tom*10000;
-    }
-    else
-    {
-        Console.WriteLine("Не удалось спарсить номер тома: " + matchesNums[0].Groups[2].Value);
-        continue;
-    }
-    
-    if (double.TryParse(matchesNums[1].Groups[4].Value, 
-            NumberStyles.Any, 
-            CultureInfo.InvariantCulture, 
-            out double chapter))
-    {
-        orderNumber += chapter;
-    }
-    else
-    {
-        Console.WriteLine("Не удалось спарсить номер главы: " + matchesNums[1].Groups[4].Value);
-        continue;
-    }
-    
-    useFile.Add(orderNumber, file);
+    Console.WriteLine("Не обнаружено файлов");
+    return 0;
 }
 
-const string OutPdfFolder = "OutPDF";
+PdfCreator pdfCreator = new();
 
-DirectoryInfo outPdfDirectory = Directory.CreateDirectory(OutPdfFolder);
-
-Console.WriteLine("Введите название выходного файлы, без расширения: ");
-
-string outFileName = Console.ReadLine();
-
-if (string.IsNullOrEmpty(outFileName))
-    outFileName = "DefaultFileName";
-
-PdfWriter writer = new PdfWriter(Path.Combine(outPdfDirectory.FullName, outFileName+".pdf"));
-
-PdfDocument pdfDocument = new PdfDocument(writer);
-Document document = new Document(pdfDocument);
-
-const string ArchiveTempFolder = "ArchiveTempFolder";
-
-foreach (var orderFileName in useFile)
+pdfCreator.Init(archiveCollection.Archives);
+string inputCommand = string.Empty;
+int command = Int32.MinValue;
+do
 {
-    Console.WriteLine("Добавляется: " + orderFileName.Value);
-    
-    SortedDictionary<int, string> tempImageFile = new SortedDictionary<int, string>();
-    
-    DirectoryInfo outArchiveDirectory = Directory.CreateDirectory(ArchiveTempFolder);
-    
-    if(outArchiveDirectory.Exists)
-        Directory.Delete(outArchiveDirectory.FullName, true);
-    
-    ZipFile.ExtractToDirectory(orderFileName.Value, outArchiveDirectory.FullName);
+    Console.WriteLine("Выберете способ создания файла: ");
+    Console.WriteLine("1 - Всё в один PDF");
+    Console.WriteLine("2 - Разделить на тома");
+    inputCommand = Console.ReadLine();
 
-    foreach (var imageFile in Directory.GetFiles(outArchiveDirectory.FullName))
-    {
-        string[] fileName = imageFile.Split('\\')[^1].Split('.');
+    int.TryParse(inputCommand, out command);
+} while (command < 1 || command > 2);
 
-        if (fileName[1].Equals("gif"))
-        {
-            Console.WriteLine("Гифку вставлять не будем");
-            continue;
-        }
-        
-        if (int.TryParse(fileName[0], out int number))
-        {
-            tempImageFile.Add(number, imageFile);
-        }
-        else
-        {
-            Console.WriteLine("Имя файла не число: " + imageFile);
-        }
-    }
+if(command == 1)
+    pdfCreator.CreateOnePdf();
+else if(command == 2)
+    pdfCreator.CreateVolumePdfs();
 
-    foreach (var imageFile in tempImageFile)
-    {
-        ImageData imageData = ImageDataFactory.Create(imageFile.Value);
-        
-        Image image = new Image(imageData);
-        image.SetWidth(pdfDocument.GetDefaultPageSize().GetWidth());
-        image.SetAutoScaleHeight(true);
-
-        document.Add(image);
-    }
-    
-    Directory.Delete(outArchiveDirectory.FullName, true);
-}
-
-pdfDocument.Close();
-
-Process.Start(new ProcessStartInfo { FileName = outPdfDirectory.FullName, UseShellExecute = true });
+Process.Start(new ProcessStartInfo { FileName = pdfCreator.GetOutDirectoryPath, UseShellExecute = true });
 
 Console.WriteLine("Усё");
+Console.ReadLine();
+
+return 1;
---
+++ ImageToPdfClassLibrary/OutFile/PdfCreator.cs
@@ -0,0 +1,139 @@
+using System.Drawing;
+using ImageToPdfClassLibrary.Archive;
+using ImageToPdfClassLibrary.Models;
+using iText.IO.Image;
+using iText.Kernel.Pdf;
+using iText.Layout;
+
+namespace ImageToPdfClassLibrary.OutFile;
+
+public class PdfCreator
+{
+    const string OutPdfFolderName = "OutPDF";
+
+    private string _outFileName;
+
+    private IEnumerable<KeyValuePair<double, ArchiveFile>> _archives;
+
+    private TempFolder _tempFolder = new();
+    
+    private DirectoryInfo _outPdfDirectory = Directory.CreateDirectory(OutPdfFolderName);
+
+    public string GetOutDirectoryPath => _outPdfDirectory.FullName;
+
+    public void Init(IEnumerable<KeyValuePair<double, ArchiveFile>> archives)
+    {
+        Console.WriteLine("Введите название общего выходного файла, без расширения: ");
+
+        _outFileName = Console.ReadLine();
+
+        if (string.IsNullOrWhiteSpace(_outFileName))
+            _outFileName = "DefaultFileName";
+
+        _outFileName = _outFileName.Trim();
+        
+        _archives = archives;
+    }
+    
+    public void CreateOnePdf()
+    {
+        PdfWriter writer = new PdfWriter(Path.Combine(_outPdfDirectory.FullName, _outFileName+".pdf"));
+
+        PdfDocument pdfDocument = new PdfDocument(writer);
+        Document document = new Document(pdfDocument);
+        
+        foreach (var orderingArchive in _archives)
+        {
+            //TODO Добавить загаловки
+            foreach (var imageFile in GetImageFiles(orderingArchive.Value.FilePath))
+            {
+                ImageData imageData = ImageDataFactory.Create(imageFile.Value);
+        
+                iText.Layout.Element.Image image = new iText.Layout.Element.Image(imageData);
+                image.SetWidth(pdfDocument.GetDefaultPageSize().GetWidth());
+                image.SetAutoScaleHeight(true);
+
+                document.Add(image);
+            }
+        }
+        
+        pdfDocument.Close();
+    }
+    
+    public void CreateVolumePdfs()
+    {
+        PdfWriter writer;
+
+        PdfDocument pdfDocument = null;
+        Document document = null;
+        
+        int currentVolume = Int32.MinValue;
+        
+        foreach (var orderingArchive in _archives)
+        {
+            if (currentVolume != orderingArchive.Value.Volume)
+            {
+                if(pdfDocument != null)
+                    pdfDocument.Close();
+                
+                writer = new PdfWriter(
+                    Path.Combine(_outPdfDirectory.FullName,
+                        _outFileName + " " + orderingArchive.Value.Volume + " Том" + ".pdf"));
+                pdfDocument = new PdfDocument(writer);
+                document = new Document(pdfDocument);
+                
+                currentVolume = orderingArchive.Value.Volume;
+            }
+            
+            //TODO Добавить загаловки
+            foreach (var imageFile in GetImageFiles(orderingArchive.Value.FilePath))
+            {
+                ImageData imageData = ImageDataFactory.Create(imageFile.Value);
+        
+                iText.Layout.Element.Image image = new iText.Layout.Element.Image(imageData);
+                image.SetWidth(pdfDocument.GetDefaultPageSize().GetWidth());
+                image.SetAutoScaleHeight(true);
+
+                document.Add(image);
+            }
+        }
+        
+        pdfDocument.Close();
+    }
+
+    private IEnumerable<KeyValuePair<int, string>> GetImageFiles(string filePath)
+    {
+        SortedDictionary<int, string> tempImageFiles = new SortedDictionary<int, string>();
+        foreach (string resourcesFile in _tempFolder.GetFilesFromZipFile(filePath))
+        {
+            string[] resourcesFilePath = resourcesFile.Split('\\');
+                
+            string[] fileName = resourcesFilePath[^1].Split('.');
+
+            string fileToAdd = resourcesFile;
+                
+            if (fileName[1].Equals("gif"))
+            {
+                // Загрузите изображение GIF
+                var gifImage = Image.FromFile(resourcesFile);
+                    
+                resourcesFilePath[^1] = fileName[0] + '.' + "jpeg";
+                fileToAdd = string.Concat(resourcesFilePath, '\\');
+                    
+                // Сохраните изображение в формате JPEG
+                gifImage.Save(fileToAdd, System.Drawing.Imaging.ImageFormat.Jpeg);
+            }
+        
+            if (int.TryParse(fileName[0], out int number))
+            {
+                tempImageFiles.Add(number, fileToAdd);
+            }
+            else
+            {
+                Console.WriteLine("Имя файла не число: " + fileToAdd);
+            }
+        }
+
+        return tempImageFiles;
+    }
+}
